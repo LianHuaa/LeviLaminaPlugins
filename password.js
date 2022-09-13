5f61214845e5b54bfef88afdd4af6989ae5cd4b2
LianHuaa <109400802+LianHuaa@users.noreply.github.com>
2022-09-13 20:05:40 +0800

    Update password.js

--- password.js
+++ password.js
@@ -1,7 +1,7 @@
 //注册插件
-ll.registerPlugin('114514登录器', '恶臭的密码登陆', [1, 1, 4]);
+ll.registerPlugin("114514登录器", "恶臭的密码登陆", [1, 1, 4]);
 //创建文件夹
-let json = new JsonConfigFile('./plugins/114514/PasswordLogin.js');
+let json = new JsonConfigFile('./plugins/1145141919810/PasswordLogin.js');
 
 //配置文档创建默认信息
 mc.listen("onJoin", function(player) {
@@ -10,7 +10,7 @@ mc.listen("onJoin", function(player) {
         var text = {
             "密码": null
         }
-        json.set(player.name, text)
+        json.set(player.name, text);
         return loginWindow(player);
     } else {
         return loginWindow(player);
@@ -28,42 +28,30 @@ function loginWindow(player) {
             var record = json.get(player.name);
             if (data == null && data == undefined) {
                 return loginWindow(player);
-            } else if (data[0] != null && data[0] != "" && data[0] != undefined) {
-                //   return loginWindow(player);
+            } else if (data[0] == "") {
+                return loginWindow(player);
             } else {
                 record["密码"] = data[0];
                 json.set(player.name, record);
             }
-            if (json.get(player.name)["密码"] != null) {
-                var cd1 = mc.newCustomForm();
-                cd1.setTitle("登录窗口");
-                cd1.addInput("输入密码");
-                player.sendForm(cd1, function(player, data) {
-                    if (data == null && data == undefined) {
-                        return loginWindow(player);
-                    } else if (data[0] != json.get(player.name)["密码"]) {
-                        return loginWindow(player);
-                    } else {
-                        player.sendToast("提示", "登陆完成")
-                    }
-                }
-                )
+            if(json.get(player.name)["密码"] != null){
+                loginWindow(player);
             }
         }
         )
     } else if (json.get(player.name)["密码"] != null) {
         var cd2 = mc.newCustomForm()
-        cd2.setTitle("登录窗口2")
+        cd2.setTitle("登录窗口")
         cd2.addInput("登录密码", "输入密码登录进入")
         player.sendForm(cd2, function(player, data) {
             if (data == null) {
                 return loginWindow(player);
-            } else if (data[0] != json.get(player.name)["密码"]) {
-                return loginWindow(player);
-            } else if (data[0] == json.get(player.name)["密码"]) {
-                player.sendToast("提示", "加入游戏")
+            } else if (data[0] == "" ){
+                    return loginWindow(player);
+                } else if (data[0] == json.get(player.name)["密码"]) {
+                    player.sendToast("提示", "加入游戏")
+                }
             }
-        }
         )
+        }
     }
-}
