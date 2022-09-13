2463af383520bec58dfbc2afc56c5437b753b7e7
LianHuaa <109400802+LianHuaa@users.noreply.github.com>
2022-09-13 11:21:57 +0800

    Create password.js

---
+++ password.js
@@ -0,0 +1,69 @@
+//注册插件
+ll.registerPlugin('114514登录器', '恶臭的密码登陆', [1, 1, 4]);
+//创建文件夹
+let json = new JsonConfigFile('./plugins/114514/PasswordLogin.js');
+
+//配置文档创建默认信息
+mc.listen("onJoin", function(player) {
+    var record = json.get(player.name);
+    if (record == null) {
+        var text = {
+            "密码": null
+        }
+        json.set(player.name, text)
+        return loginWindow(player);
+    } else {
+        return loginWindow(player);
+    }
+}
+);
+
+
+function loginWindow(player) {
+    if (json.get(player.name)["密码"] == null) {
+        var cd = mc.newCustomForm();
+        cd.setTitle("注册窗口");
+        cd.addInput("输入密码", "输入密码注册");
+        player.sendForm(cd, function(player, data) {
+            var record = json.get(player.name);
+            if (data == null && data == undefined) {
+                return loginWindow(player);
+            } else if (data[0] != null && data[0] != "" && data[0] != undefined) {
+                //   return loginWindow(player);
+            } else {
+                record["密码"] = data[0];
+                json.set(player.name, record);
+            }
+            if (json.get(player.name)["密码"] != null) {
+                var cd1 = mc.newCustomForm();
+                cd1.setTitle("登录窗口");
+                cd1.addInput("输入密码");
+                player.sendForm(cd1, function(player, data) {
+                    if (data == null && data == undefined) {
+                        return loginWindow(player);
+                    } else if (data[0] != json.get(player.name)["密码"]) {
+                        return loginWindow(player);
+                    } else {
+                        player.sendToast("提示", "登陆完成")
+                    }
+                }
+                )
+            }
+        }
+        )
+    } else if (json.get(player.name)["密码"] != null) {
+        var cd2 = mc.newCustomForm()
+        cd2.setTitle("登录窗口2")
+        cd2.addInput("登录密码", "输入密码登录进入")
+        player.sendForm(cd2, function(player, data) {
+            if (data == null) {
+                return loginWindow(player);
+            } else if (data[0] != json.get(player.name)["密码"]) {
+                return loginWindow(player);
+            } else if (data[0] == json.get(player.name)["密码"]) {
+                player.sendToast("提示", "加入游戏")
+            }
+        }
+        )
+    }
+}
